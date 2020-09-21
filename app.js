9649b2bebd862ed60e5c5eddc965472e04c468b5
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-09-21 23:29:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+let rndm = Math.floor(Math.random() * 101); /* Rasgele sayı üretmek için Math (0 dahil 1   hariç)*/
+document.getElementById("btn").addEventListener("click",check)
 
+function check() {
+    let num = document.getElementById("text").value;
+    if (rndm == num) {
+        alert("bildin");    
+    }else if (rndm > num) {
+        alert("biraz yukarı");
+    }else {
+        alert("biraz aşağıya");
+    } 
+}
