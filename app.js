334c16a2ecd801591eb282b80ba1f5fd62776e61
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-09-22 00:36:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
 let rndm = Math.floor(Math.random() * 101); /* Rasgele sayı üretmek için Math (0 dahil 1   hariç)*/
 document.getElementById("btn").addEventListener("click",check)
 
+let counter = 0;
+
+let message = document.getElementById('message');
+let counter_p = document.getElementById('number');
+
+
 function check() {
-    let num = document.getElementById("text").value;
+
+    let num = Number(document.getElementById("text").value);
+    
+    if(isNaN(num) || num == ''){
+        if(message.style.display="none"){
+            message.style.display="block";
+            message.innerText="Are you sure that this is a number?";
+        }else{
+            message.innerText="Are you sure that this is a number?";
+        }
+        return;
+    }
+
     if (rndm == num) {
-        alert("bildin");    
+        message.innerText="Congrats!!"
+        message.style.display = 'block';
+        console.log(num) 
+        console.log(rndm) 
+
     }else if (rndm > num) {
-        alert("biraz yukarı");
+        message.innerText="Biraz yukari cik"
+        message.style.display = 'block'; 
+        console.log(num) 
+        console.log(rndm) 
+
+
     }else {
-        alert("biraz aşağıya");
+        message.innerText="Biraz asagi in"
+        message.style.display = 'block'; 
+        console.log(num) 
+        console.log(rndm) 
+
     } 
+
+    counter++;
+    counter_p.style.display = "block";
+    counter_p.innerText = `This is your test number: ${counter}`;
+
+
 }
