f964c59e6f3e16710b2c9a6eade78bbe174a5e8d
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-09-22 15:19:20 +0200

    Finished but it can be  improved

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 let rndm = Math.floor(Math.random() * 101); /* Rasgele sayı üretmek için Math (0 dahil 1   hariç)*/
 document.getElementById("btn").addEventListener("click",check)
 
-let counter = 0;
-
 let message = document.getElementById('message');
 let counter_p = document.getElementById('number');
+let input =  document.getElementById("text");
+let counter = 0;
 
 
 function check() {
 
-    let num = Number(document.getElementById("text").value);
+    let num = Number(input.value);
     
     if(isNaN(num) || num == ''){
         if(message.style.display="none"){
@@ -22,29 +22,26 @@ function check() {
     }
 
     if (rndm == num) {
-        message.innerText="Congrats!!"
-        message.style.display = 'block';
-        console.log(num) 
-        console.log(rndm) 
+        counter++;
+        window.alert('You are the winner :D. You guest after 29 times!')
+        if(window.confirm('You wanna play again?')){
+            window.location.reload();
+        }else{
+            window.close();
+        }
 
     }else if (rndm > num) {
-        message.innerText="Biraz yukari cik"
-        message.style.display = 'block'; 
-        console.log(num) 
-        console.log(rndm) 
-
-
+        message.innerText="I am thinking about higher number";
+        
     }else {
-        message.innerText="Biraz asagi in"
-        message.style.display = 'block'; 
-        console.log(num) 
-        console.log(rndm) 
-
-    } 
+        message.innerText="I am thinking about lower number";
+        
+    }
 
     counter++;
-    counter_p.style.display = "block";
     counter_p.innerText = `This is your test number: ${counter}`;
+    input.focus();
+    input.value = '';
 
 
 }
